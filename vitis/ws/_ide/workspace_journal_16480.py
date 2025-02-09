0cce965b284921cd2e5d9b2a0e0279dede2a5277
Andrew Fan <andrewkongfan@gmail.com>
2025-02-09 00:13:27 -0800

    merge speedups

---
+++ vitis/ws/_ide/workspace_journal_16480.py
@@ -0,0 +1,80 @@
+# 2025-02-08T21:34:44.442279700
+import vitis
+
+client = vitis.create_client()
+client.set_workspace(path="ws")
+
+platform = client.get_component(name="platform")
+status = platform.build()
+
+comp = client.get_component(name="app_component")
+comp.build()
+
+status = platform.build()
+
+comp = client.get_component(name="app_component_xor")
+comp.build()
+
+status = platform.build()
+
+comp.build()
+
+status = platform.build()
+
+comp.build()
+
+status = platform.build()
+
+comp.build()
+
+status = platform.build()
+
+comp.build()
+
+status = platform.build()
+
+comp.build()
+
+status = platform.build()
+
+comp.build()
+
+status = platform.build()
+
+comp.build()
+
+status = platform.build()
+
+comp.build()
+
+status = platform.build()
+
+comp.build()
+
+status = platform.build()
+
+comp.build()
+
+status = platform.build()
+
+comp.build()
+
+status = platform.build()
+
+comp = client.get_component(name="uart_listener")
+comp.build()
+
+status = platform.build()
+
+comp.build()
+
+status = platform.build()
+
+comp.build()
+
+status = platform.build()
+
+comp.build()
+
+vitis.dispose()
+
